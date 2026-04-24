41abdf5cd7208583320b447383dc5b5bc85441ff
Castillo-Fernando <xxferrunoxx@gmail.com>
2026-04-24 04:32:58 -0700

    refactor: streamline database connection and server startup

--- index.js
+++ index.js
@@ -2,20 +2,10 @@ const express = require('express');
 const pool = require('./db');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API funcionando');
-});
-
-pool.connect()
-  .then(() => {
-    console.log('Conexión exitosa a PostgreSQL');
-  })
-  .catch((err) => {
-    console.error('Error de conexión', err);
-  });
-
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
 });
 
 app.get('/alumnos', async (req, res) => {
@@ -26,4 +16,8 @@ app.get('/alumnos', async (req, res) => {
     console.error('Error al consultar alumnos:', error);
     res.status(500).json({ error: 'Error al obtener los alumnos' });
   }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
