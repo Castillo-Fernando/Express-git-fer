bb567f9e9a609767442fe5e0b5b759e928681cf3
Castillo-Fernando <xxferrunoxx@gmail.com>
2026-04-24 04:51:04 -0700

    feat: add endpoint to retrieve user information

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.get('/alumnos', async (req, res) => {
   }
 });
 
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Juan',
+    rol: 'Administrador'
+  };
+
+  res.json(usuario);
+});
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
