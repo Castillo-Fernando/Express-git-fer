77d1e23fd092ea5a7027495f18b13bc5b6ec2efc
Castillo-Fernando <xxferrunoxx@gmail.com>
2026-04-24 04:31:46 -0700

    ruta agregada

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ pool.connect()
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
+});
